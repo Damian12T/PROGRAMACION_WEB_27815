90d827e7ed9ea64baccb29e0dcbfd31c4477d4d8
Edgar Damián Toscano Coro <damiantoscano101@gmail.com>
2025-12-17 12:13:08 -0500

    objeto

--- PRY_OBJETOS/public/js/objetos.js
+++ PRY_OBJETOS/public/js/objetos.js
@@ -1,11 +1,11 @@
 // declarar un object
 
 var persona = {
-    cedula : "1716519513",
-    nombres: "Edgar Damian",
-    apellidos: "Toscano Coro",
-    edad : 21,
-    estatura: 1.70,
+    cedula: "1726192618",
+    nombres: "EDGAR DAMIAN",
+    apellidos: "TOSCANO CORO",
+    edad: 21,
+    estatura: 1.81,
     es_ecuatoriano: true,
     mis_asignaturas: [
                         'programacion web',
@@ -13,16 +13,11 @@ var persona = {
                         'metodos numericos'
                     ],
     fn_saludar: function(){
-        console.log(`Hola mi nombre es ${this.nombres} ${this.apellidos}`);
+        return `Hola mi nombre es ${this.nombres} ${this.apellidos}`;
     },
 
     fn_calcularEdad:function(){
-      //  console.log(this.edad>=18 ?'Es mayor de edad': 'Es menor de edad');
-    
-    if(this.edad>=18){
-        return 'Es mayor de edad';
-    }else{
-        return 'Es menor de edad';
+        console.log(this.edad>=18 ?'Es mayor de edad': 'Es menor de edad');
     }
 
 
@@ -30,4 +25,7 @@ var persona = {
 //persona.cedula;
 //persona.mis_asignaturas[1];
 //persona.fn_saludar();
-persona.fn_calcularEdad();
+//persona.fn_calcularEdad();
+
+var header_info = document.getElementById('header_info');
+header_info.innerHTML=`${persona.fn_saludar()}`
