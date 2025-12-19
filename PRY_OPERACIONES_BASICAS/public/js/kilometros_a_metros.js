4ac35dfca9635af306b6c397dac1cccfbfb6fd1e
Edgar Damián Toscano Coro <damiantoscano101@gmail.com>
2025-12-18 22:59:15 -0500

    Removiendo node_modules del repositorio

--- PRY_OPERACIONES_BASICAS/public/js/kilometros_a_metros.js
+++
@@ -1,9 +0,0 @@
-
-var btn_kil= document.getElementById('btn_kil');
-btn_kil.addEventListener('click',function(event){
-let n1 =document.getElementById('kil_1').value;
-let resultado=document.getElementById('kil_resultado');
-resultado.innerHTML=parseFloat(n1)*(1000);
-
-
-});
